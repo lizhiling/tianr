910297d3fc3c52382b3cfd80fca9cbe0455592a5
Li Zhiling <li_zhil@worksap.co.jp>
2018-04-20 12:56:38 +0800

    modify docker control

--- docker.sh
+++ docker.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 mvn clean
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 mvn package spring-boot:repackage
 mvn dockerfile:build
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+TAG=`date +%Y%m%d`
+docker tag dylzl678/tianr:latest dylzl678/tianr:${TAG}
+docker push dylzl678/tianr:${TAG}
 docker push dylzl678/tianr:latest
